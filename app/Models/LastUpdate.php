7ae350c5103876b7aff41a4369b0457e1af6b951
Hoang Nguyen <nbhoangit@gmail.com>
2020-06-18 21:21:56 +0700

    feat(api): store last time update data

---
+++ app/Models/LastUpdate.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LastUpdate extends Model
+{
+
+    const AirTempType = 'at';
+    const PSIType = 'psi';
+
+   /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+   protected $fillable = [
+       'type', 'time'
+   ];
+}
